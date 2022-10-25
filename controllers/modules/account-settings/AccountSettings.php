f77aa15cdb5b94ddbc5cd7ac26f4351261fa5148
Juan Vanegas <eatteer@gmail.com>
2022-10-24 23:56:06 -0500

    Refactor submitted data establishment of values and errors

--- controllers/modules/account-settings/AccountSettings.php
+++ controllers/modules/account-settings/AccountSettings.php
@@ -11,12 +11,9 @@ class AccountSettingsController
 
   function __construct()
   {
-    /**
-     * Get the id of the authenticated user
-     * to find him in the database and initialize the form
-     * with his data
-     */
     $this->validator = new Validator;
+    /* Get the id of the authenticated user to find him in the database
+    and initialize the form with his data */
     $userId = $_SESSION["userId"];
     $user = UserModel::findById($userId);
     $this->viewData = [
@@ -50,12 +47,13 @@ class AccountSettingsController
     $wasSubmitted = isset($_POST["updateBasicInformation"]);
     $this->viewData["basicInformationForm"]["wasSubmitted"] = $wasSubmitted;
 
-
-    /**
-     * If the form was not submitted just render the page
-     */
+    /* If the form was not submitted just render the page */
     if (!$wasSubmitted) return;
 
+    /* Set form values with submitted values */
+    $this->viewData["basicInformationForm"]["values"] = $_POST;
+
+    /* Validate submitted values */
     $validation = $this->validator->validate($_POST, [
       "name" => "required",
       "surname" => "required",
@@ -63,73 +61,26 @@ class AccountSettingsController
       "password" => "required"
     ]);
 
+    /* Set form errors */
     if ($validation->fails()) {
-      $errors = $validation->errors();
-      ChromePhp::log(json_encode($errors->firstOfAll()));
+      $generatedErrors = $validation->errors()->firstOfAll();
+      $errors = $this->viewData["basicInformationForm"]["errors"];
+      $this->viewData["basicInformationForm"]["errors"] = array_merge($errors, $generatedErrors);
+      return;
     }
 
-    // Validate form to set view data
-    // Get submitted values
-    $name = $_POST["name"];
-    $surname = $_POST["surname"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    // Set form values
-    $this->viewData["basicInformationForm"]["values"]["name"] = $name;
-    $this->viewData["basicInformationForm"]["values"]["surname"] = $surname;
-    $this->viewData["basicInformationForm"]["values"]["email"] = $email;
-    $this->viewData["basicInformationForm"]["values"]["password"] = $password;
-
-    // Set form errors
-    if (empty($name)) {
-      $this->viewData["basicInformationForm"]["errors"]["name"] = "Name is required";
-    }
-
-    if (empty($surname)) {
-      $this->viewData["basicInformationForm"]["errors"]["surname"] = "Surname is required";
-    }
-
-    if (empty($email)) {
-      $this->viewData["basicInformationForm"]["errors"]["email"] = "Email is required";
-    }
-
-    if (empty($password)) {
-      $this->viewData["basicInformationForm"]["errors"]["password"] = "Password is required";
-    }
-
-    // FORM HAS ERRORS
-    // Do not continue with the code below the foreach
-    // until the value of the errors array is empty
-    $registerFormErrors = $this->viewData["basicInformationForm"]["errors"];
-    foreach ($registerFormErrors as $value) {
-      if (!empty($value)) return;
-    }
-
-    /**
-     * Update the user data with the submitted data
-     */
-    $userData = [
-      "name" => $name,
-      "surname" => $surname,
-      "email" => $email,
-      "password" => $password
-    ];
+    /* Try to update user data */
     $userId = $_SESSION["userId"];
-    $wasSuccess = UserModel::update($userId, $userData);
+    $wasSuccess = UserModel::update($userId, $_POST);
 
-    /**
-     * In case an error occurs, set an error message
-     */
+    /* In case an error occurs, set an error message */
     if (!$wasSuccess) {
       $this->viewData["basicInformationForm"]["errorMessage"] = "Something went wrong";
       return;
     }
 
-    /**
-     * If the user information update could be done, set {informationSuccessfullyUpdated}
-     * to true to display a toast on the view
-     */
+    /* If the user information update could be done, set {informationSuccessfullyUpdated}
+    to true to display a toast on the view */
     $this->viewData["basicInformationForm"]["informationSuccessfullyUpdated"] = true;
   }
 }
