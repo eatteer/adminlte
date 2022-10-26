ad8c20945b5fe77d16e33237f6416084443055e2
Juan Vanegas <eatteer@gmail.com>
2022-10-26 09:59:44 -0500

    Update database schema with roles and permissions

--- database-schema.sql
+++ database-schema.sql
@@ -20,6 +20,30 @@ DROP DATABASE IF EXISTS `adminlte`;
 CREATE DATABASE IF NOT EXISTS `adminlte` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `adminlte`;
 
+-- Dumping structure for table adminlte.permissions
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table adminlte.permissions: ~0 rows (approximately)
+DELETE FROM `permissions`;
+
+-- Dumping structure for table adminlte.roles
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table adminlte.roles: ~0 rows (approximately)
+DELETE FROM `roles`;
+
 -- Dumping structure for table adminlte.users
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -27,16 +51,48 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `state` enum('active','inactive') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table adminlte.users: ~2 rows (approximately)
 DELETE FROM `users`;
 INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `state`) VALUES
-	(13, 'Juan Esteban', 'Vanegas Osorio', 'juan@gmail.com', '123', 'active'),
-	(15, 'Kevin Alejandro', 'Sotto Guengue', 'kevin@gmail.com', '123', 'active');
+	(23, 'Juan Esteban', 'Vanegas Osorio', 'juan@gmail.com', '$2y$10$SfQVWrR6Qw2OESGlX/cy3Ow4JcLExl332gK4RU1ZyPBEkfNQkBKne', 'active'),
+	(24, 'Kevin Alejandro', 'Sotto Guengue', 'kevin@gmail.com', '$2y$10$eV3xruPXkH2UqePn7Clc9eqTmpAK2NrNS/bXnc4eiKp6knQ4rxcwu', 'active');
+
+-- Dumping structure for table adminlte.users_permissions
+DROP TABLE IF EXISTS `users_permissions`;
+CREATE TABLE IF NOT EXISTS `users_permissions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  KEY `fk_permission_id` (`permission_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_permission_id` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table adminlte.users_permissions: ~0 rows (approximately)
+DELETE FROM `users_permissions`;
+
+-- Dumping structure for table adminlte.users_roles
+DROP TABLE IF EXISTS `users_roles`;
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  KEY `fk_rol_id` (`rol_id`),
+  CONSTRAINT `fk_rol_id` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table adminlte.users_roles: ~0 rows (approximately)
+DELETE FROM `users_roles`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
